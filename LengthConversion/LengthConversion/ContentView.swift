b996762ff4654b8c39ac9199e4357f68ec3f9581
galih <samudragalih94@gmail.com>
2023-12-28 21:05:55 +0700

    feat(Convert): update add logic to convert from input length to output length and add `Section` to show output length

--- LengthConversion/LengthConversion/ContentView.swift
+++ LengthConversion/LengthConversion/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var originalAmount = 0.0
-    @State private var originalLength = "m"
+    @State private var originalLength = Length.m.rawValue
+    @State private var outputLength = Length.m.rawValue
     
     enum Length: String {
         case m, km, feet, yards, miles
@@ -21,16 +22,25 @@ struct ContentView: View {
             case .km:
                 return 1000
             case .feet:
-                return 3.28084
+                return 0.3048
             case .yards:
-                return 1.0936133333333
+                return 0.9144
             case .miles:
-                return 0.00062137121212119323429
+                return 1_609.344
             }
         }
     }
     
     let lenghts = [Length.m.rawValue, Length.km.rawValue, Length.feet.rawValue, Length.yards.rawValue, Length.miles.rawValue]
+    
+    private var outputAmount: Double {
+        let ogLength = Length(rawValue: originalLength) ?? Length.m
+        let input = originalAmount * ( ogLength.numberValue / Length.m.numberValue )
+        let otLength = Length(rawValue: outputLength) ?? Length.m
+        let output = input / otLength.numberValue
+        
+        return output
+    }
     
     var body: some View {
         Form {
@@ -38,6 +48,16 @@ struct ContentView: View {
                 TextField("Input Amount", value: $originalAmount, format: .number)
                     .keyboardType(.decimalPad)
                 Picker("Original Length Conversion", selection: $originalLength) {
+                    ForEach(lenghts, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Output Length") {
+                Text(outputAmount, format: .number)
+                Picker("Original Length Conversion", selection: $outputLength) {
                     ForEach(lenghts, id: \.self) {
                         Text($0)
                     }
