4ca7895da84023a3980331c4b905ac6d16ca31c8
galih <samudragalih94@gmail.com>
2023-12-28 19:41:04 +0700

    wip: initial push project

---
+++ LengthConversion/LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Galih Samudra on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
