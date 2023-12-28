a96e1079ae2775fd20269e557035c703b15b777a
galih <samudragalih94@gmail.com>
2023-12-28 20:20:11 +0700

    feat(UI): add `Section` `Picker` to select original length

--- LengthConversion/LengthConversion/ContentView.swift
+++ LengthConversion/LengthConversion/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var originalAmount = 0.0
+    @State private var originalLength = "m"
+    
+    let lenghts = ["m", "km", "feet", "yards", "miles"]
     
     var body: some View {
         Form {
-            Section("Original Amount") {
+            Section("Original Length") {
                 TextField("Input Amount", value: $originalAmount, format: .number)
                     .keyboardType(.decimalPad)
-                
+                Picker("Original Length Conversion", selection: $originalLength) {
+                    ForEach(lenghts, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
             }
         }
     }
