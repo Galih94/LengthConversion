52fd88b543213a6fbdd693a6370b23ce385622b7
galih <samudragalih94@gmail.com>
2023-12-28 20:33:11 +0700

    feat(Length): create `enum` Length to put lengths raw value and number value

--- LengthConversion/LengthConversion/ContentView.swift
+++ LengthConversion/LengthConversion/ContentView.swift
@@ -11,11 +11,30 @@ struct ContentView: View {
     @State private var originalAmount = 0.0
     @State private var originalLength = "m"
     
-    let lenghts = ["m", "km", "feet", "yards", "miles"]
+    enum Length: String {
+        case m, km, feet, yards, miles
+        
+        var numberValue: Double {
+            switch self {
+            case .m:
+                return 1
+            case .km:
+                return 1000
+            case .feet:
+                return 3.28084
+            case .yards:
+                return 1.0936133333333
+            case .miles:
+                return 0.00062137121212119323429
+            }
+        }
+    }
+    
+    let lenghts = [Length.m.rawValue, Length.km.rawValue, Length.feet.rawValue, Length.yards.rawValue, Length.miles.rawValue]
     
     var body: some View {
         Form {
-            Section("Original Length") {
+            Section("Input Length") {
                 TextField("Input Amount", value: $originalAmount, format: .number)
                     .keyboardType(.decimalPad)
                 Picker("Original Length Conversion", selection: $originalLength) {
